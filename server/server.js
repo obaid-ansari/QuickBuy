b77624b4545a3ab308c036325fb5161bfd2eae17
Obaid Ansari <ansari.ubaid.1020@gmail.com>
2026-04-24 16:09:28 +0530

    cors issue update

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["https://quickbuysite.netlify.app", "http://localhost:5173"],
     credentials: true,
   }),
 );
